37d5b2d43ecaac6c43f136d754b098d807feb7f7
Ekaterina Potapova <samarityanka.kate@gmail.com>
2018-08-28 22:04:11 +0300

    Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TestCore;
+
+namespace Task5
+{
+    //Разработать консольное приложение.
+    //С клавиатуры вводится произвольная строка.
+    //Вывести количество слов в строке.Учесть, что слова могут быть разделены пробелами или знаками препинания.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+
+            char[] delimiters = new char[] { ' ', '.', ',', '!', '?' , '—' };
+            var res = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+            Console.WriteLine($"Count of words {res}");
+            MessageHelper.AnyKey();
+
+        }
+    }
+}
