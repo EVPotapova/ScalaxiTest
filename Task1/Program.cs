9e2309671ba0f44ce66f383c1528e88029349503
Ekaterina Potapova <samarityanka.kate@gmail.com>
2018-08-28 18:33:17 +0300

    Tasks 1 and 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using TestCore;
 
 namespace Task1
 {
+    //Разработать консольное приложение.
+    //С клавиатуры последовательно вводятся данные: имя, фамилия, возраст, рост, вес. 
+    //Вывести данные одной строкой по центру.
+    //Использовать форматный вывод.
+    //Использовать интерполированные строки.
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int nameMaxLength = 35;
+            const int numberMaxLength = 3;
+
+
+            //TODO: Message
+            string name = Console.ReadLine().Truncate(nameMaxLength);
+            string surname = Console.ReadLine().Truncate(nameMaxLength);
+            string age = Console.ReadLine().Truncate(numberMaxLength);
+            string height = Console.ReadLine().Truncate(numberMaxLength);
+            string weight = Console.ReadLine().Truncate(numberMaxLength);
+
+
+            var formatResult = string.Format("{0} {1} {2} {3} {4}", name, surname, age, height, weight);
+            var interResult = $"{name} {surname} {age} {height} {weight}";
+            Console.SetCursorPosition((Console.WindowWidth - formatResult.Length) / 2, Console.CursorTop);
+            Console.WriteLine(formatResult);
+            Console.SetCursorPosition((Console.WindowWidth - interResult.Length) / 2, Console.CursorTop);
+            Console.WriteLine(interResult);
+            Console.ReadKey();
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task2
+{
+    //Разработать консольное приложение.Ввести с клавиатуры число n, вывести n-ый член последовательность Фибоначчи.
+    //Вычислить, используя рекурсию.
+    //Вычислить без рекурсии.
+    //(*) Вычислить, используя формулу Бине.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string positionStr = Console.ReadLine();
+            var res = int.TryParse(positionStr, out int position);
+            if (!res || position < 1)
+            {
+                //TODO: Message
+                Console.ReadLine();
+                return;
+            }
+
+            int left = 1;
+            int right = 1;
+            int pos = 2;
+            int sum = 0;
+            while (pos < position)
+            {
+                sum = left + right;
+                left = right;
+                right = sum;
+                pos += 1;
+            }
+            Console.WriteLine($"Without recursion {sum}");
+
+            Console.WriteLine($"With recursion {Fib(position)}");
+
+
+            //Формула Бине
+            var bSum = (Math.Pow(((1 + Math.Sqrt(5)) / 2), position) - Math.Pow(((1 - Math.Sqrt(5)) / 2), position)) / (Math.Sqrt(5));
+
+
+            Console.WriteLine($"Binet {bSum}");
+            Console.ReadLine();
+        }
+
+        private static int Fib(int position)
+        {
+            if (position == 1 || position == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(position - 1) + Fib(position - 2);
+            }
+        }
+    }
+}
