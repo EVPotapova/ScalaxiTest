5ca811411a1670215cd20da05d2fbadc4f2a9d40
Ekaterina Potapova <samarityanka.kate@gmail.com>
2018-08-29 18:18:18 +0300

    Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,11 +1,23 @@
 ﻿//Разработать консольное приложение.
 //    С клавиатуры вводится произвольная строка.Проверить, что она представляет собой корректный ip-адрес(IPv4).
+using System;
+using System.Text.RegularExpressions;
+using TestCore;
+
 namespace Task7
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string str = Console.ReadLine();
+
+            //Без указания длины маски подсети
+            Regex ip = new Regex(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+
+            Console.WriteLine(ip.IsMatch(str.Trim()) ? "Correct" : "Incorrect");
+
+            MessageHelper.AnyKey();
         }
     }
 }
