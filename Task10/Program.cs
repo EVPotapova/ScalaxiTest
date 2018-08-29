f4f77a8812862381ea470cc0473b62d6e66adff1
Ekaterina Potapova <samarityanka.kate@gmail.com>
2018-08-29 17:40:15 +0300

    Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,11 +1,27 @@
 ﻿//(*) Разработать консольное приложение.С клавиатуры вводится число.Вывести 0, если это число 0, иначе вывести 1. 
 //    Не использовать явным образом циклы, сравнения и условные операторы.
+using System;
+using TestCore;
+
 namespace Task10
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //TODO: TO Core
+            string numberStr = Console.ReadLine();
+            if (!int.TryParse(numberStr, out var number))
+            {
+
+                Console.WriteLine("Incorrect number");
+                MessageHelper.AnyKey();
+                return;
+            }
+
+            Console.WriteLine(Convert.ToInt32(Convert.ToBoolean(number)));
+
+            MessageHelper.AnyKey();
         }
     }
 }
